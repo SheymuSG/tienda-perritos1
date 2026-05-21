be3138aa71bc27d461a3ce461009f46c2242f86a
SheymuSG <eltobalguzman@gmail.com>
2026-05-21 14:48:40 -0400

    fix: actualizar IP y credenciales de la base de datos en el backend

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,13 @@ const mysql = require("mysql2/promise");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const {
-  DB_HOST = "10.0.2.134", // acá colocar la IP Privada EC2 DB
-  DB_USER = "root",
-  DB_PASSWORD = "admin123",
-  DB_NAME = "tienda_perritos",
-  DB_PORT = 3306,
-} = process.env;
-
+const connection = mysql.createConnection({
+  host: '172.31.37.224',       
+  user: 'alumno',              
+  password: 'alumno123',       
+  database: 'tienda_perritos', 
+  port: 3306                   
+});
 app.use(cors());
 app.use(express.json());
 
